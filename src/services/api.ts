6889c3f0c8f859250cc06261ac6835efae947611
larissagiaccon <larissa_souz@hotmail.com>
2021-04-13 20:17:46 -0300

    Desafio Fundamentos React Native

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
+  // baseURL: 'http://192.168.50.145:3333',
   baseURL: 'http://localhost:3333',
 });
 
 export default api;
+
+// yarn json-server --host 192.168.50.145 server.json -p 3333
